028fce2cbec3cdac43f9a1dcbf06b17d828c2952
Miguel Ceja <mikealexcd@gmail.com>
2021-01-04 23:45:15 -0500

    Added background to top sections and reduced the space when scrolling on Desktop

--- src/Photography.js
+++ src/Photography.js
@@ -7,7 +7,7 @@ import Page from './Page';
 export default function Photography() {
     return (
       <div>
-      <Page sectionid="photos" title="Photography" />
+      <Page sectionid="photos" title="Photography" className="photo-bg text-white shadow" />
       <WobbleChairs />
       {/* <SmallObjects /> */}
     </div>
--- src/Webdev.js
+++ src/Webdev.js
@@ -7,7 +7,7 @@ import WebsiteExamples from './web-examples/WebsiteExamples';
 export default function WebDev() {
     return (
       <div>
-      <Page sectionid="Websites" title="Web Development" />
+      <Page sectionid="Websites" title="Web Development" className="webdev-bg text-white" />
       <ShowCasePage title="Websites" name="Website Deployment" previous="top" description="These are some examples of the websites that I've worked on professionally. The e-commerce sites were made using Magento, and the blogs were deployed with Wordpress. Sites related to CBD are still in construction as the companies themselves are still being figured out (overall branding, products, packaging prices, availability). All of these sites were deployed on AWS, through different services such as EC2, S3, SES, Route 53, etc.">
       <WebsiteExamples />
       </ShowCasePage>
