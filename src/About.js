1dc8090113f61a96526c235c4daf8c5654e1dbca
mikealexcd <mikealexcd@gmail.com>
2021-07-06 14:51:58 -0400

    Change interests
    I'm now into coding

--- src/About.js
+++ src/About.js
@@ -9,7 +9,7 @@ export default function AboutMe(props) {
         <div className="full-container d-flex flex-column align-items-center" id={props.title}>
         <ShowCasePage image={me} previous="top" next="false">
         <h2 className="mb-4">About me</h2>
-                <p>Recent Honours Graduate from the Bachelor of Commerce – Digital Business Management program at Humber College in Toronto. I am a passionate web developer looking to learn new technologies and apply them in both my personal projects and a professional setting. I’m into tech, noodles, and photography.</p>
+                <p>Recent Honours Graduate from the Bachelor of Commerce – Digital Business Management program at Humber College in Toronto. I am a passionate web developer looking to learn new technologies and apply them in both my personal projects and a professional setting. I’m into tech, noodles, and coding.</p>
         </ShowCasePage>
         </div>
     )
